95022238b43ba708dc24c36e0ca0988986e073cd
이은찬 <jsmisth26@gmail.com>
2026-01-26 23:15:31 +0900

    feat: save the user's face in detections.bounding_box if face detected.

--- face_recog.py
+++ face_recog.py
@@ -19,12 +19,14 @@ VisionRunningMode = vision.RunningMode
 options = FaceDetectorOptions(
     base_options=BaseOptions(model_asset_path=MODEL_PATH),
     running_mode=VisionRunningMode.IMAGE,
-    min_detection_confidence=0.6,
+    min_detection_confidence=0.3,
 )
 detector = FaceDetector.create_from_options(options)
 
 # 웹캠에서 프레임 1장 캡쳐
 cap = cv2.VideoCapture(0)
+for _ in range(10):
+    cap.read()
 if not cap.isOpened():
     raise RuntimeError("Unable to open the Camera")
 ret, frame = cap.read()
@@ -39,3 +41,24 @@ image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
 
 detection_result = detector.detect(image)
 print(detection_result)
+
+
+# 얼굴 인식 테스트용 코드
+# if detection_result.detections:
+#     detection = detection_result.detections[0]
+#     bbox = detection.bounding_box
+
+#     x = bbox.origin_x
+#     y = bbox.origin_y
+#     w = bbox.width
+#     h = bbox.height
+
+#     face_img = frame[y:y+h, x:x+w]
+
+#     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+#     save_path = os.path.join(CAPTURE_DIR, f"face_{timestamp}.jpg")
+
+#     cv2.imwrite(save_path, face_img)
+#     print("Saved:", save_path)
+# else:
+#     print("No face detected")
