009a83cb6be2b6bb8dd9e6a8583a1f89c21d962d
이은찬 <jsmisth26@gmail.com>
2026-01-30 16:26:38 +0900

    feat: add owner face embedding pipeline

--- face_embedder.py
+++ face_embedder.py
@@ -1,3 +1,5 @@
+import os
+import cv2
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
@@ -5,7 +7,6 @@ import numpy as np
 
 def embed_face_rgb(face_rgb):
     np.set_printoptions(threshold=np.inf)
-
     MODEL_PATH = "models/mobilenet_v3_small_075_224_embedder.tflite"
     base_options = python.BaseOptions(model_asset_path=MODEL_PATH)
     options = vision.ImageEmbedderOptions(
@@ -18,3 +19,37 @@ def embed_face_rgb(face_rgb):
 
         embedding = result.embeddings[0].embedding
         return np.array(embedding)
+
+# 주인 얼굴의 평균 벡터값
+def build_owner_embedding(
+        owner_faces_dir: str = 'data/owner_faces', 
+        output_path: str = 'data/owner_embedding.npy'
+    ):
+    # data/owner_faces의 파일들을 임베딩 -> 각 벡터값들의 평균벡터 추출
+    embeddings = []
+    for file in os.listdir(owner_faces_dir):
+        if not file.lower().endswith(('.jpg', '.jpeg')):
+            continue
+        image_path = os.path.join(owner_faces_dir, file)
+        bgr_image = cv2.imread(image_path)
+
+        if bgr_image is None:
+            print(f"[WARNING] Fail to load Image: {file}")
+            continue
+        rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
+        embedding = embed_face_rgb(rgb_image)
+
+        if embedding is None or embedding.size == 0:
+            print(f"[WARNING] Fail to embed image: {file}")
+        embeddings.append(embedding)
+        print(f"[SUCCESS] Embedding Completed: {file}")
+
+    owner_embeddings = np.mean(embeddings, axis=0)
+    np.save(output_path, owner_embeddings)
+    
+
+
+
+
+
+
