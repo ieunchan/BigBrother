52b710b349a12ad0922e06475602bc0721c79ca9
이은찬 <jsmisth26@gmail.com>
2026-02-19 23:29:30 +0900

    fix: git ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from face_logics.face_embedder import build_owner_embedding
 from face_logics.face_detector import run_capture_and_save
 
-run_capture_and_save()
-build_owner_embedding()
+# run_capture_and_save()
+# build_owner_embedding()
